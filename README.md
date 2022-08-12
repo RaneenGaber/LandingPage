eabf5b74dd4c96c9c37a886ee03294ef448fcf6f
Raneen Gaber <78453166+RaneenGaber@users.noreply.github.com>
2022-08-13 00:21:14 +0200

    add active  state

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # Landing Page Project
+This is my first project, and I hope to be successful in using the tools that I learned to implement what is required of me in this project.
 
-## Table of Contents
+I have added the sections via HTML.
 
-* [Instructions](#instructions)
+I created a function to create a link for each section.
 
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Modified the active class properties for both section and link.
--- js/app.js
+++ js/app.js
@@ -1,81 +1,79 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-const myNav = document.getElementById('navbar__list');
+const myNav = document.getElementById("navbar__list");
+
 const mySections = document.querySelectorAll("section");
 console.log(mySections.length);
-  // loop through each section
- for (let i = 0; i < mySections.length; i++) {
-   const myList = document.createElement("li");
-   const myAnchor = document.createElement("a");
-   myAnchor.className = "menu__link";
-   myAnchor.setAttribute("href","#section" + (i+1));
-
-   myAnchor.textContent = "section" + (i + 1);
-   myList.appendChild(myAnchor);
-   myNav.appendChild(myList);
- }
-
+// loop through each section
+for (let i = 0; i < mySections.length; i++) {
+  const myList = document.createElement("li");
+  const myAnchor = document.createElement("a");
+  myAnchor.className = "menu__link";
+  myAnchor.setAttribute("href", `#section${i + 1}`);
+  myAnchor.textContent = `section${i + 1}`;
+  myList.appendChild(myAnchor);
+  myNav.appendChild(myList);
+}
 
 // Add class 'active' to section when near top of viewport
 
+const myAnchors = document.querySelectorAll("a");
+
 // on window scroll
-window.addEventListener('scroll', (e) => {
-
-
+window.addEventListener("scroll", (e) => {
   // loop through each section
-  mySections.forEach( section => {
-
+  for (let i = 0; i < mySections.length; i++) {
     // get px distance from top
-    const topDistance = section.getBoundingClientRect().top;
+    const topDistance = mySections[i].getBoundingClientRect().top;
 
     // if the distance to the top is between 0-100px
     if (topDistance > 0 && topDistance < 100) {
-      section.classList.add("your-active-class");
+      // Set sections as active
+      mySections[i].classList.add("your-active-class");
+      myAnchors[i].classList.add("active");
 
-    // otherwise, remove the class
+      // otherwise, remove the class
     } else {
-      section.classList.remove("your-active-class");
+      mySections[i].classList.remove("your-active-class");
+      myAnchors[i].classList.remove("active");
     }
-  });
+  }
 });
 
 // Scroll to anchor ID using scrollTO event
@@ -83,23 +81,18 @@ window.addEventListener('scroll', (e) => {
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
   });
 });
-
-// Set sections as active
-
-
